78ad51486189498107a902370dd98d19f5072835
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-09-23 17:38:43 +0200

    display and allow editing of blocked submissions and comments.

--- src/components/SubmissionsTable.tsx
+++ src/components/SubmissionsTable.tsx
@@ -1,20 +1,24 @@
 import { AgGridReact } from "ag-grid-react";
-import { AgColumn, AllCommunityModule, ModuleRegistry, type ColDef } from "ag-grid-community";
+import { AgColumn, AllCommunityModule, ModuleRegistry, type CellClickedEvent, type CellEditingStoppedEvent, type ColDef } from "ag-grid-community";
 import { useEffect, useState } from "react";
-import type { CourseType, SubmissionType } from "../types/EntityTypes";
+import type { CourseType } from "../types/EntityTypes";
 import moodleService from "../utils/moodleService";
 import moment from "moment";
 import Spinner from "./Spinner";
 import Select from "./Select";
 import { useSearchParams } from "react-router-dom";
+import storageService from "../utils/storageService";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 type RowItemType = {
+    submissionId: number
     course: string,
     assignment: string,
     learner: number,
     status: string,
+    blocked: boolean,
+    comment: string,
     action: string,
     moduleId: number,
     courseId: number,
@@ -33,14 +37,17 @@ export default function SubmissionTable() {
 
 
     const [colDefs] = useState<ColDef[]>([
+        { field: 'submissionId', hide: true },
         {
             field: 'course',
             cellStyle: { color: "#0084d1", cursor: "pointer" }
         },
-        { field: 'assignment', cellStyle: { color: "#0084d1", cursor: "pointer" } },
+        { field: 'assignment', cellStyle: { color: "#0084d1", cursor: "pointer" }, flex: 1 },
         { field: 'learner', width: 100, resizable: false, flex: 0 },
-        { field: 'submitted', valueFormatter: (param) => moment(param.value).fromNow() },
+        { field: 'submitted', width: 200, resizable: false, flex: 0, valueFormatter: (param) => moment(param.value).fromNow() },
         { field: 'status', width: 100, resizable: false, flex: 0 },
+        { field: 'blocked', width: 100, resizable: false, flex: 0, editable: true },
+        { field: "comment", editable: (params) => params.data.blocked, cellStyle: { color: "red" } },
         { field: 'action', width: 150, resizable: false, flex: 0, cellStyle: { color: "#0084d1", cursor: "pointer" }, sortable: false },
         { field: 'moduleId', hide: true },
         { field: 'courseId', hide: true },
@@ -50,42 +57,55 @@ export default function SubmissionTable() {
     const defaultColDef = { flex: 1 };
 
     useEffect(() => {
-        try {
-            setLoading(true)
-            moodleService.getSubmissions().then((data: SubmissionType[]) => {
-                const submissions: RowItemType[] = data.filter(sub => sub.gradingStatus !== 'graded' && sub.status == 'submitted').map((submission) => ({
-                    course: submission.courseName,
-                    assignment: submission.assignmentName,
-                    learner: submission.userId,
-                    submitted: new Date(submission.submittedAt),
-                    status: submission.status,
-                    action: 'Grade',
-                    moduleId: submission.coursModuleId,
-                    courseId: submission.courseId,
-                    learnerId: submission.userId
-                }))
-                setRowData(submissions)
-                setLoading(false)
-            })
-        } catch (error) {
-            setLoading(false)
-        }
-
         // courses
-        try {
-            setCoursesLoading(true)
-            moodleService.getCourses().then(data => setCourses(data))
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setCoursesLoading(false)
+        setCoursesLoading(true)
+        moodleService.getCourses()
+            .then(data => {
+                setCourses(data)
+                setCoursesLoading(false)
+            })
+            .catch(err => {
+                console.log(err);
+                setCoursesLoading(false)
+            })
+
+        async function fetchData() {
+            // submissions
+            setLoading(true)
+            try {
+                const submissions = await moodleService.getSubmissions()
+                const blockedSubmissions = await storageService.getAllBlockedSubmissions()
+                const submissionRows: RowItemType[] = submissions.filter(sub => sub.gradingStatus !== 'graded' && sub.status == 'submitted').map((submission) => {
+                    const blockedSubmission = blockedSubmissions.find((item => item.submission_id == submission.id))
+                    return {
+                        submissionId: submission.id,
+                        course: submission.courseName,
+                        assignment: submission.assignmentName,
+                        learner: submission.userId,
+                        submitted: new Date(submission.submittedAt),
+                        status: submission.status,
+                        blocked: !!blockedSubmission?.id,
+                        comment: blockedSubmission?.comment || "",
+                        action: 'Grade',
+                        moduleId: submission.coursModuleId,
+                        courseId: submission.courseId,
+                        learnerId: submission.userId
+                    }
+                })
+                setRowData(submissionRows)
+                setLoading(false)
+            } catch (error) {
+                console.log(error);
+                setLoading(false)
+            }
         }
+        fetchData()
     }, [])
 
     return (
         <div>
-            <form className="max-w-sm mb-5">
-                <label className="mb-1 block">Select a Course</label>
+            <div className="max-w-sm mb-5">
+                <p className="mb-1 block">Select a Course</p>
                 <Select name="course"
                     onSelect={(value) => setSearchParams((searchParams) => {
                         searchParams.set("course", value);
@@ -93,7 +113,7 @@ export default function SubmissionTable() {
                     })}
                     loading={coursesLoading}
                     options={courses ? [{ label: 'All courses', id: 0, value: 0 }, ...courses.map((c: CourseType) => ({ label: c.name, id: c.id, value: c.id }))] : []} />
-            </form>
+            </div>
             <div style={{ width: "100%", height: 500 }}>
                 {loading &&
                     <div className="text-center">
@@ -105,7 +125,7 @@ export default function SubmissionTable() {
                     rowData={rowData.filter(item => (!parseInt(searchParams.get('course') || '') ? true : item.courseId == parseInt(searchParams.get('course') || '')))}
                     columnDefs={colDefs}
                     defaultColDef={defaultColDef}
-                    onCellClicked={(event) => {
+                    onCellClicked={(event: CellClickedEvent) => {
                         const data = event.data
                         const column: AgColumn | any = event.column
 
@@ -115,6 +135,25 @@ export default function SubmissionTable() {
                             window.open(`https://moodle.shaper.co.za/mod/assign/view.php?id=${data.moduleId}`, "_blank");
                         if (column.colId == 'course')
                             window.open(`https://moodle.shaper.co.za/course/view.php?id=${data.courseId}`, "_blank");
+                    }}
+                    onCellEditingStopped={(event: CellEditingStoppedEvent) => {
+                        const column: AgColumn | any = event.column
+
+                        if (column.colId == 'blocked') {
+                            if (event.oldValue === false)
+                                storageService
+                                    .createBlockedSubmission(event.data.submissionId)
+                                    .catch(err => { console.log(err) })
+                            else
+                                storageService
+                                    .removeBlockedSubmission(event.data.submissionId)
+                                    .catch(err => { console.log(err) })
+                        }
+
+                        if (column.colId == 'comment')
+                            storageService
+                                .updateBlockedSubmissionComment(event.data.submissionId, event.newValue)
+                                .catch(err => { console.log(err) })
                     }}
                 />}
             </div>
