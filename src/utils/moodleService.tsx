7e12c44b1c1e9efced80ac29c1c7c316739dd9a0
AdamDono <flipbrickzmusic1@gmail.com>
2026-03-31 12:54:13 +0200

    feat: optimize SubmissionsTable to use the new single ungraded submissions API

--- src/utils/moodleService.tsx
+++ src/utils/moodleService.tsx
@@ -10,7 +10,8 @@ const WSFunctions = {
     mod_quiz_get_user_quiz_attempts: "mod_quiz_get_user_quiz_attempts",
     mod_quiz_get_quiz_required_qtypes: "mod_quiz_get_quiz_required_qtypes",
     core_course_get_enrolled_users_by_cmid: "core_course_get_enrolled_users_by_cmid",
-    mod_quiz_get_user_attempts: "mod_quiz_get_user_attempts"
+    mod_quiz_get_user_attempts: "mod_quiz_get_user_attempts",
+    local_grades_get_ungraded_submissions: "local_grades_get_ungraded_submissions"
 }
 
 function getUrl(wsfunction: string) {
@@ -247,4 +248,37 @@ async function getQuizSubmissions() {
 
     return result
 }
-export default { getCourses, getAssignmentSubmissions, getQuizSubmissions }
+
+export type UngradedSubmissionType = {
+    id: string; // The a240 or q123 string
+    coursename: string;
+    activitytype: string;
+    activityname: string;
+    username: string;
+    timemodified: number;
+    coursepath: string;
+    activitypath: string;
+    gradepath: string;
+}
+
+async function getUngradedSubmissions(): Promise<UngradedSubmissionType[]> {
+    let response;
+    try {
+        response = await fetch(getUrl(WSFunctions.local_grades_get_ungraded_submissions))
+    } catch (error) {
+        console.log(`Error: Could not fetch ungraded submissions`, error);
+        return []
+    }
+
+    if (!response.ok) {
+        console.log(`Error: Could not fetch ungraded submissions HTTP error`, response.status)
+        return []
+    }
+
+    const result: UngradedSubmissionType[] = await response.json();
+    if (!Array.isArray(result)) return [];
+    
+    return result;
+}
+
+export default { getCourses, getAssignmentSubmissions, getQuizSubmissions, getUngradedSubmissions }
